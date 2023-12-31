8016d1389c07043a52b5dec4d1995d3e07cc0a7a
Rachel <bb50305030@gmail.com>
2023-12-31 23:41:39 +0200

    change name paramater

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Hello from './Hello'
 function App() {
   const [count, setCount] = useState(0);
   console.log("hi i'm mali");
-  let c='Mali user';
+  let code='Mali user';
 
   return (<><h1>hi!</h1>
   <Hello/></>
